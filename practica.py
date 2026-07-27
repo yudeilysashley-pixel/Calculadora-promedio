dc09d954f420f1edd428384149dc6040499bd8c8
yudeilysashley-pixel <yudeilysashley@gmail.com>
2026-07-27 13:41:50 -0400

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,9 @@
+calif1= float (input("Ingrese calif1:"))
+calif2= float (input("Ingrese calif2:"))
+calif3= float (input("Ingrese calif3:"))
+calif4= float (input("Ingrese calif4:"))
+promedio= (calif1+calif2+calif3+calif4)/4
+if promedio >70:
+    print ("Promovido. promedio:",promedio)
+else:
+    ("Reprobado. promedio:",promedio)
